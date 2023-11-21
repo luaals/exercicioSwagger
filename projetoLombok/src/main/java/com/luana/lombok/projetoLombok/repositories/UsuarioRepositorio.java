c22ebc616386f16bfd7cfc9c1764683998990987
luaals <134397630+luaals@users.noreply.github.com>
2023-11-21 09:12:23 -0300

    Delete projetoLombok directory

--- projetoLombok/src/main/java/com/luana/lombok/projetoLombok/repositories/UsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.luana.lombok.projetoLombok.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luana.lombok.projetoLombok.entities.Usuario;
-
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
-
-}
